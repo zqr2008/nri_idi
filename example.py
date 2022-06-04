fa8ac2e1163a6fdbbc962e2ef65212d343dfb67c
zqr2008 <73268508+zqr2008@users.noreply.github.com>
2022-06-04 11:50:43 +0800

    Update example.py

--- example.py
+++ example.py
@@ -2,5 +2,6 @@ from nri_f import nri
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv(r"C:\Users\mjdee\Desktop\nri\example.csv")
-nri(df,"old_model","new_model","gold_std")
+df = pd.read_csv(r"C:\Users\mjdee\Desktop\nri\tablefor_NRI.csv")
+df_output = nri(df,"saps_pro","xgboost","alive")
+df_output.to_csv(r"C:\Users\mjdee\Desktop\nri\result_NRI.csv")
